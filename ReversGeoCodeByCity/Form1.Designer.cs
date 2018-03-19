20c9f05c8486cc3f985eec9f63702ecc77d59617
amurati <ahmet.murati@live.com>
2018-03-19 15:09:58 +0100

    Add files via upload

---
+++ ReversGeoCodeByCity/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace ReversGeoCodeByCity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.txtProcessesed = new System.Windows.Forms.TextBox();
+            this.txtUnprocessed = new System.Windows.Forms.TextBox();
+            this.txtPercentage = new System.Windows.Forms.TextBox();
+            this.txtGoogleMapsKey = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pBPercentage = new System.Windows.Forms.ProgressBar();
+            this.txtExcel = new System.Windows.Forms.TextBox();
+            this.lblPerc = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoad.Location = new System.Drawing.Point(39, 27);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 43);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Ngarko";
+            this.toolTip1.SetToolTip(this.btnLoad, "Ngarko skedarin Excel");
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Enabled = false;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Location = new System.Drawing.Point(347, 21);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 43);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Nis";
+            this.toolTip1.SetToolTip(this.btnStart, "Ngarko skedarin Excel");
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtCount
+            // 
+            this.txtCount.BackColor = System.Drawing.Color.White;
+            this.txtCount.Location = new System.Drawing.Point(115, 167);
+            this.txtCount.Name = "txtCount";
+            this.txtCount.ReadOnly = true;
+            this.txtCount.Size = new System.Drawing.Size(78, 20);
+            this.txtCount.TabIndex = 4;
+            this.toolTip1.SetToolTip(this.txtCount, "Numri i rreshtave në \r\nskedarin Excel");
+            // 
+            // txtProcessesed
+            // 
+            this.txtProcessesed.BackColor = System.Drawing.Color.White;
+            this.txtProcessesed.Location = new System.Drawing.Point(199, 167);
+            this.txtProcessesed.Name = "txtProcessesed";
+            this.txtProcessesed.ReadOnly = true;
+            this.txtProcessesed.Size = new System.Drawing.Size(78, 20);
+            this.txtProcessesed.TabIndex = 5;
+            this.toolTip1.SetToolTip(this.txtProcessesed, "Numri i rreshtave të gjetur");
+            // 
+            // txtUnprocessed
+            // 
+            this.txtUnprocessed.BackColor = System.Drawing.Color.White;
+            this.txtUnprocessed.Location = new System.Drawing.Point(283, 167);
+            this.txtUnprocessed.Name = "txtUnprocessed";
+            this.txtUnprocessed.ReadOnly = true;
+            this.txtUnprocessed.Size = new System.Drawing.Size(78, 20);
+            this.txtUnprocessed.TabIndex = 6;
+            this.toolTip1.SetToolTip(this.txtUnprocessed, "Numri i rreshtave të papërpunuar");
+            // 
+            // txtPercentage
+            // 
+            this.txtPercentage.BackColor = System.Drawing.Color.White;
+            this.txtPercentage.Location = new System.Drawing.Point(367, 167);
+            this.txtPercentage.Name = "txtPercentage";
+            this.txtPercentage.ReadOnly = true;
+            this.txtPercentage.Size = new System.Drawing.Size(78, 20);
+            this.txtPercentage.TabIndex = 7;
+            this.toolTip1.SetToolTip(this.txtPercentage, "Përqindja");
+            this.txtPercentage.TextChanged += new System.EventHandler(this.txtPercentage_TextChanged);
+            // 
+            // txtGoogleMapsKey
+            // 
+            this.txtGoogleMapsKey.Location = new System.Drawing.Point(6, 31);
+            this.txtGoogleMapsKey.Name = "txtGoogleMapsKey";
+            this.txtGoogleMapsKey.Size = new System.Drawing.Size(335, 20);
+            this.txtGoogleMapsKey.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.txtGoogleMapsKey);
+            this.groupBox1.Location = new System.Drawing.Point(29, 91);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(432, 70);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Google Maps Key";
+            // 
+            // pBPercentage
+            // 
+            this.pBPercentage.Location = new System.Drawing.Point(39, 224);
+            this.pBPercentage.Name = "pBPercentage";
+            this.pBPercentage.Size = new System.Drawing.Size(513, 28);
+            this.pBPercentage.TabIndex = 3;
+            // 
+            // txtExcel
+            // 
+            this.txtExcel.Location = new System.Drawing.Point(138, 39);
+            this.txtExcel.Name = "txtExcel";
+            this.txtExcel.Size = new System.Drawing.Size(335, 20);
+            this.txtExcel.TabIndex = 8;
+            // 
+            // lblPerc
+            // 
+            this.lblPerc.AutoSize = true;
+            this.lblPerc.BackColor = System.Drawing.Color.Transparent;
+            this.lblPerc.Location = new System.Drawing.Point(280, 239);
+            this.lblPerc.Name = "lblPerc";
+            this.lblPerc.Size = new System.Drawing.Size(24, 13);
+            this.lblPerc.TabIndex = 9;
+            this.lblPerc.Text = "0 %";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(596, 282);
+            this.Controls.Add(this.lblPerc);
+            this.Controls.Add(this.txtExcel);
+            this.Controls.Add(this.txtPercentage);
+            this.Controls.Add(this.txtUnprocessed);
+            this.Controls.Add(this.txtProcessesed);
+            this.Controls.Add(this.txtCount);
+            this.Controls.Add(this.pBPercentage);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnLoad);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximumSize = new System.Drawing.Size(616, 325);
+            this.MinimumSize = new System.Drawing.Size(616, 325);
+            this.Name = "Form1";
+            this.Opacity = 0.85D;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            this.MouseHover += new System.EventHandler(this.Form1_MouseHover);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox txtGoogleMapsKey;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ProgressBar pBPercentage;
+        private System.Windows.Forms.TextBox txtCount;
+        private System.Windows.Forms.TextBox txtProcessesed;
+        private System.Windows.Forms.TextBox txtUnprocessed;
+        private System.Windows.Forms.TextBox txtPercentage;
+        private System.Windows.Forms.TextBox txtExcel;
+        private System.Windows.Forms.Label lblPerc;
+    }
+}
+
---
+++ ReversGeoCodeByCity/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ReversGeoCodeByCity
+{
+    public partial class Form1 : Form
+    {
+        /*
+         * emri i skedarit 
+         */
+         string naked;
+
+        /*
+         *  Skedari i Excel
+         */
+        private String excelFile;
+
+        /*
+         *  Nurmi i rreshtave
+         */
+        decimal rowCount;
+
+        /*
+        *  Nurmi i rreshtave të përpunuar
+        */
+        decimal processed;
+
+        /*
+        *  Nurmi i rreshtave të papërunuar
+        */
+        decimal unprocessed;
+
+        /*
+        *  Përqindja
+        */
+        decimal percentage;
+
+        /**
+         *  Aplikacioni i Excel
+         */
+        private Microsoft.Office.Interop.Excel.Application xlApp;
+
+        /*
+         *Workbook i skedarit Excel 
+         */
+        private Workbook xlWorkbook;
+
+        /*
+         Shtresa në të cilën do të punojmë
+             */
+        private _Worksheet xlWorksheet;
+
+        /*
+         * Rangu në të cilën i qasemi qelizave për të lexuar dhe shkruar.
+         */
+        private Range xlRange;
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+            rowCount = 0;
+           
+        }
+
+        private void OpenExcel()
+        {
+
+    
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+               
+               
+              
+                ofd.Filter = "Excel |*.xls;*.xlsx"; // Filtri për t'i filtruar vetëm skedarët e Excel
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        excelFile = ofd.FileName; // marrja e shtegut të plotë të skedarit të përzgjedhur nga dialogu
+                        naked = Path.GetFileNameWithoutExtension(excelFile); // nxjerrja e emrit nga shtegu
+                        txtExcel.Text = naked;
+                        btnStart.Enabled = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void Proceso( )
+        {
+            string country = "";
+            string city = "";
+            int i = 2 ;
+
+            Process.GetCurrentProcess().PriorityBoostEnabled = true; // Aftësimi i përforcimit të prioritetit të procesit aktual
+            Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.RealTime; // Caltimi i prioritetit të procesit aktual në RealTime 
+
+            // Of course this only affects the main thread rather than child threads. 
+            Thread.CurrentThread.Priority = ThreadPriority.Highest;
+            if (File.Exists(excelFile)) // kontrollim nëse skedari ekziston, masë parandaluese
+                                 // në rast se rastësisht skedari fshihet ose disku ose usb memoria dëmtohet ose largohet.
+            {
+                xlApp = new Microsoft.Office.Interop.Excel.Application();
+
+                //Krimi i objekteve COM. Krijon një objekt COM për çdo gjë që referencohet
+
+                xlWorkbook = xlApp.Workbooks.Open(excelFile);
+                xlWorksheet = xlWorkbook.Sheets[1];
+                xlRange = xlWorksheet.UsedRange;
+
+                rowCount = xlRange.Rows.Count;
+                setTotalCount();
+                city = "";
+                //qarkullimi nëpër rreshta dhe qeliza për lexim dhe shkrim të vlerës sonë të fituar
+                //excel nuk është i bazuar nga zero!! i fillon nga 2 që të anashkalohet titulli
+                for (i = 2; i <= rowCount - 2; i++)
+                {
+
+                    var cell1 = xlRange.Cells[i, 1]; // nxjerrja e të dhënës në formë objekti COM në rreshtin i dhe shtyllën 1
+                    city = cell1.Value2.ToString(); // konvertimi i formës COM në string
+                    if (city.Length > 0) // Nëse Emërtim i qytetit mungon atëherë anashkalohet
+                    {
+                        try
+                        {
+                            country = LookForCountry(city); // Kërkon për emrin e shtetit
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                            break;
+                        }
+                        if (country.Length > 0)
+                        {
+                            processed++;
+
+                            unprocessed = rowCount - processed;
+                            percentage =  (processed / rowCount);
+                        }
+                       
+                        setProcessed();
+                        setUnprocessed();
+                        setPercentage();
+                        xlRange.Cells[i, 2] = country; // Nëse gjendet e vëmë emrin e shtetit në qelizën e dytë të rreshtit i.
+                    }
+                }
+            }
+            Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.Normal; // Pas kryerjes së punës e kthejmë 
+                                                                                     //vlerën e prioritet në prioritet normal
+
+            // Of course this only affects the main thread rather than child threads.
+            Thread.CurrentThread.Priority = ThreadPriority.Normal;
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); // marrja e shtegut të Desktop.
+            xlWorkbook.SaveAs(Path.Combine(path, "Exported_" + naked + ".xlsx")); // ruajtja e materialit të përpunuar në emër të ri 
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            //mbyllja dhe lirimi i objetkit
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            //mbyllja dhe lirimi i objektit 
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+        }
+
+        private void setTotalCount()
+        {
+            txtCount.Text = rowCount.ToString("N0");
+        }
+
+        private void setProcessed()
+        {
+            txtProcessesed.Text = processed.ToString("N0");
+        }
+        private void setUnprocessed()
+        {
+            txtUnprocessed.Text = unprocessed.ToString("N0");
+        }
+
+        private void setPercentage()
+        {
+              
+            txtPercentage.Text = String.Format("{0:P2}", percentage);
+            pBPercentage.Value = (int)(percentage * 100);
+            lblPerc.Text = String.Format("{0:P2}", percentage); 
+        }
+
+        /*
+         * Kjo metodë e merr emrin e qytetit dhe e pason kërkesën te google api për marrjen e JSON aktual
+         */
+        private String GetJSON(string name)
+        {
+            String json = "";
+            if (txtGoogleMapsKey.TextLength > 0)
+            {
+                json = new WebClient().
+                    DownloadString("https://maps.googleapis.com/maps/api/geocode/json?address=" +
+                    name + "&sensor=false&key=" + txtGoogleMapsKey.Text);
+            }
+            return json;
+        }
+
+        /*
+         * Metodë për kërkimin e shtetit dhe nxjerrja e asaj informate nëse ekziston ai shtet.
+         */
+        private String LookForCountry(String name)
+        {
+            RootObject obj = new RootObject(); // objekti ku do të ruhen të dhënat nga JSON
+            string c = ""; // ndryshojra c që e ruan emrin e shtetit të gjendur ose jo.
+            string json = GetJSON(name); // ruhet JSON i kërkuar në bazë të emrin
+            if (!json.Contains("error_message") && !json.Equals("<empty>") && json.Length > 0)
+            {// kontrollohet nëse JSON ka ndonjë gabim dhe është bosh dhe  për gjatësi më të madhe se zero të stringut
+                try // provohet 
+                {
+                    // kthimi i json në objekt - Deserializimi
+                    var ob2 = (RootObject)JsonConvert.DeserializeObject<RootObject>(json);
+
+                    int cd = 0; // ndryshore për numërim të rreshtave të komponentës Address_component.
+                    foreach (var item in ob2.results) // përçdo item të ob2.results
+                    {
+                        bool found = false; // ndryshore nëse gjendet shteti 
+
+                        foreach (var ac in item.address_components) // qarkullim nëpër address_commponents
+                        {
+                            if (item.address_components[cd].types[0].ToString() == "country")
+                            { // nëse gjendet komponenta e adresës e cila llojin e ka "country"
+                                // atëherë lexohet emri i plotë i shtetit
+                                c = item.address_components[cd].long_name;
+
+                                found = true; // ndryshorja found bëhet true
+                                break; // ndërpritet qarkullimi nëpër item.adress_components
+                            }
+                            cd++; // e rrit vlerën për një
+                        }
+                        if (found) // nëse është gjetur
+                        {
+                            cd = 0; // vlera e numratorit rikthehet në zero
+                            break; // ndërpritet qarkullimi npër ob2.results
+                        }
+                    }
+                }
+                catch (Exception)
+                {// nëse nuk gjendet 
+                    c = " Not found ";
+                }
+
+
+            }
+            else
+            {
+                var msg = (ErrorMessage)JsonConvert.DeserializeObject<ErrorMessage>(json);
+                throw new Exception(msg.error_message);
+            }
+            return c;
+        }
+
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (txtGoogleMapsKey.TextLength > 0)
+            {
+                OpenExcel(); // hapja e skedarit Excel
+            }
+            else
+            {
+                MessageBox.Show("Duhet ta jepni çelësin e Google Maps");
+            }
+        }
+
+        private void txtPercentage_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Proceso();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Drawing.Point p = pBPercentage.Location;
+            int x, y;
+            x  = ((int)p.X + (pBPercentage.Width / 2)) - ((int)lblPerc.Width / 2);
+            y = ((int)p.Y +(pBPercentage.Height / 2)) - ((int)lblPerc.Height / 2);
+             
+            System.Drawing.Point p2 = new System.Drawing.Point(x, y);
+            lblPerc.Location = p2; 
+        }
+
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void Form1_MouseHover(object sender, EventArgs e)
+        {
+            this.Opacity = 100;
+        }
+
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                this.Opacity = 100 - i;
+
+            }
+        }
+    }
+
+    public class ErrorMessage
+    {
+        public string error_message { get; set; }
+        public List<String> results { get; set; }
+        public string status { get; set; }
+    }
+}
